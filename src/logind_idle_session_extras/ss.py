62913efb2064f0a6a601c0e38edca9ff0b6f1591
Louis Wust <louis.w.wust@nasa.gov>
2024-11-14 11:16:27 -0800

    Phase 2 of finding VNC connections complete

--- src/logind_idle_session_extras/ss.py
+++ src/logind_idle_session_extras/ss.py
@@ -4,17 +4,11 @@
 from ipaddress import IPv4Address, IPv6Address, ip_address
 import re
 import subprocess
-from typing import Collection, Mapping, NamedTuple, Tuple, Union
+from typing import Collection, Mapping, NamedTuple, Optional, Tuple, Union
 
 
-class ProcessSocket(NamedTuple):
-    """Represent an established TCP socket belonging to a local process"""
-
-    # An IPv4Address or IPv6Address associated with the local socket
-    addr: Union[IPv4Address, IPv6Address]
-
-    # TCP port number associated with the established TCP socket
-    port: int
+class SocketProcess(NamedTuple):
+    """Represent a single local process associated with a Socket"""
 
     # Short name of the process image (e.g., "sshd") bound to this socket
     comm: str
@@ -25,9 +19,16 @@ class ProcessSocket(NamedTuple):
     # File descriptor number within the process representing this socket
     fd: int
 
+    def __eq__(self, other):
+        if not isinstance(other, SocketProcess):
+            return False
+        return (self.comm == other.comm and
+                self.pid == other.pid and
+                self.fd == other.fd)
 
-class LoopbackConnection:
-    """Represent a loopback TCP connection between two LocalSockets
+
+class Socket(NamedTuple):
+    """Represent a socket potentially associated with several processes
 
     Perhaps confusingly, Linux (and UNIX) allow multiple processes to share a
     single socket in the abstract socket namespace. This is very common in the
@@ -35,123 +36,183 @@ class LoopbackConnection:
     descriptor representing the socket and also endows the same to its child.
     That's why more than one ProcessSocket object can participate in both
     sides.
-
-    Any LoopbackConnection object created by this ss.py Python module will
-    guarantee that all of the client[*].port values are equal to one another,
-    and that all of the server[*].port values are equal to one another.
     """
 
-    # One or more processes bound to the "client" side of the connection
-    client: Collection[ProcessSocket]
+    # An IPv4Address or IPv6Address associated with the local socket
+    addr: Union[IPv4Address, IPv6Address]
 
-    # One or more processes bound to the "server" side of the connection
-    server: Collection[ProcessSocket]
+    # TCP port number associated with the established TCP socket
+    port: int
+
+    # Zero or more SocketProcesses associated with the local socket
+    processes: Collection[SocketProcess]
 
     def __eq__(self, other):
-        if not isinstance(other, LoopbackConnection):
+        if not isinstance(other, Socket):
             return False
+        elif self.addr != other.addr:
+            return False
+        elif self.port != other.port:
+            return False
+        elif len(self.processes) != len(other.processes):
+            return False
+        else:
+            other_processes = list(other.processes)
+            for my_process in self.processes:
+                try:
+                    other_idx = other_processes.find(my_process)
+                    del other_processes[other_idx]
+                except ValueError:
+                    return False
+            return True
 
-        my_client_set = frozenset(self.client)
-        other_client_set = frozenset(other.client)
-        my_server_set = frozenset(self.server)
-        other_server_set = frozenset(other.server)
+class LoopbackConnection(NamedTuple):
+    """Represent a loopback TCP connection between two LocalSockets"""
 
-        # During the original computation, it is difficult to tell initially
-        # whether a given TCP connection is oriented one way (local = client
-        # and peer = server) or the other way (local = server and peer =
-        # client). Allow these cases to be easily identified by considering a
-        # LoopbackConnection to be equivalent regardless of this orientation.
+    # One or more processes bound to the "client" side of the connection
+    client: Socket
 
-        return ((my_client_set == other_client_set and
-                 my_server_set == other_server_set) or
-                (my_client_set == other_server_set and
-                 my_server_set == other_client_set))
+    # One or more processes bound to the "server" side of the connection
+    server: Socket
 
 
-def _obtain_raw_ss_data() -> Tuple[Collection[ProcessSocket],
-                                   Mapping[ProcessSocket,
-                                           Tuple[Union[IPv4Address,
-                                                       IPv6Address],
-                                                 int]]]:
-    """Use the 'ss' command to obtain data for identifying LoopbackConnections
+class _SSInvocation:
+    """Collect context derived from an invocation of of the 'ss' command
 
-    This is the first phase of a multi-phase operation. At the end of this
-    function, a tuple is returned with two elements. The first is a set of
-    ProcessSockets which are known to be listening.
-
-    The second is a map of ProcessSockets which are known to be part of an
-    established connection; to the peer addresses and port numbers they are
-    connected to.
+    Users are not intended to instantiate this class directly. Please just see
+    the find_loopback_connections function below, instead.
     """
 
-    cp = subprocess.run(["/usr/sbin/ss",
-                         "--all",
-                         "--no-header",
-                         "--numeric",
-                         "--oneline",
-                         "--processes",
-                         "--tcp"],
-                        encoding='utf-8',
-                        stdout=subprocess.PIPE)
+    # A set of Sockets which are known to be listening.
+    _listen_sockets: Collection[Socket]
 
-    # LISTEN 0 128 0.0.0.0:22 0.0.0.0:* users:(("sshd",pid=5533,fd=3))
-    socket_re = re.compile(r'^(?P<State>[-A-Z]+)\s+'
-                           r'(?P<RecvQ>\d+)\s+'
-                           r'(?P<SendQ>\d+)\s+'
-                           r'\[?(?P<LocalAddress>[:.0-9]+)\]?:'
-                           r'(?P<LocalPort>\d+)\s+'
-                           r'\[?(?P<PeerAddress>[:.0-9]+)\]?:'
-                           r'(?P<PeerPort>\d+|\*)\s*'
-                           r'(users:\((?P<Process>.*)\))?\s*$')
+    # Associations of Sockets which are known to be part of an established
+    # connection, along with the peer address and port numbers they are
+    # connected to.
+    _established_sockets: Collection[Tuple[Socket,
+                                           Union[IPv4Address,
+                                                 IPv6Address],
+                                           int]]
 
-    # ("rpcbind",pid=4935,fd=4),("systemd",pid=1,fd=327)
-    paren_re = re.compile(r'[()]')
-    comm_re = re.compile(r'^"(.*)"$')
-    pid_re = re.compile(r'^pid=(\d+)$')
-    fd_re = re.compile(r'^fd=(\d+)$')
+    # A collection of LocalConnections which have been extracted from the
+    # previously-populated _established_sockets.
+    _loopback_connections: Collection[LoopbackConnection]
 
-    listen_sockets = []
-    established_sockets = {}
+    def __init__(self):
+        self._listen_sockets = []
+        self._established_sockets = []
+        self._loopback_connections = []
 
-    for socket_line in cp.stdout.splitlines():
-        socket_match = socket_re.match(socket_line)
-        if socket_match is None:
-            raise ValueError('invalid socket spec detected in output of ss: "{}"',
-                             socket_line)
+    def _step_1_obtain_raw_ss_data(self):
+        """Run 'ss' and populate the initial collections
 
-        if socket_match.group('Process') is not None:
-            process_clause = socket_match.group('Process')
-            process_without_parens = paren_re.sub('', process_clause)
-            process_parts = process_without_parens.split(',')
+        This is the first phase of a multi-phase operation. At the end of this
+        phase, the _listen_sockets will be populated and so will the
+        _established_sockets. The latter will contain ALL established
+        connections for now.
+        """
 
-            if len(process_parts) % 3 != 0:
-                raise ValueError('invalid structure for process clause: "{}"',
-                                 process_clause)
+        cp = subprocess.run(["/usr/sbin/ss",
+                            "--all",
+                            "--no-header",
+                            "--numeric",
+                            "--oneline",
+                            "--processes",
+                            "--tcp"],
+                            encoding='utf-8',
+                            stdout=subprocess.PIPE)
 
-            for base in range(0, len(process_parts) // 3):
-                individual_parts = process_parts[base*3:(base+1)*3]
+        # LISTEN 0 128 0.0.0.0:22 0.0.0.0:* users:(("sshd",pid=5533,fd=3))
+        socket_re = re.compile(r'^(?P<State>[-A-Z]+)\s+'
+                            r'(?P<RecvQ>\d+)\s+'
+                            r'(?P<SendQ>\d+)\s+'
+                            r'\[?(?P<LocalAddress>[:.0-9]+)\]?:'
+                            r'(?P<LocalPort>\d+)\s+'
+                            r'\[?(?P<PeerAddress>[:.0-9]+)\]?:'
+                            r'(?P<PeerPort>\d+|\*)\s*'
+                            r'(users:\((?P<Process>.*)\))?\s*$')
 
-                comm_match = comm_re.match(individual_parts[0])
-                pid_match = pid_re.match(individual_parts[1])
-                fd_match = fd_re.match(individual_parts[2])
-                if comm_match is None or pid_match is None or fd_match is None:
-                    raise ValueError('invalid structure for process clause: "{}"',
-                                    process_clause)
+        # ("rpcbind",pid=4935,fd=4),("systemd",pid=1,fd=327)
+        paren_re = re.compile(r'[()]')
+        comm_re = re.compile(r'^"(.*)"$')
+        pid_re = re.compile(r'^pid=(\d+)$')
+        fd_re = re.compile(r'^fd=(\d+)$')
 
-                individual_socket = ProcessSocket(
+        for socket_line in cp.stdout.splitlines():
+            socket_match = socket_re.match(socket_line)
+            if socket_match is None:
+                raise ValueError('invalid socket spec detected: "{}"',
+                                socket_line)
+
+            if socket_match.group('Process') is not None:
+                individual_socket = Socket(
                     addr=ip_address(socket_match.group('LocalAddress')),
                     port=int(socket_match.group('LocalPort')),
-                    comm=comm_match.group(1),
-                    pid=int(pid_match.group(1)),
-                    fd=int(fd_match.group(1))
+                    processes=[]
                 )
 
-                if socket_match.group('State') == 'LISTEN':
-                    listen_sockets.append(individual_socket)
-                elif socket_match.group('State') == 'ESTAB':
-                    established_sockets[individual_socket] = (
-                            ip_address(socket_match.group('PeerAddress')),
-                            int(socket_match.group('PeerPort'))
-                    )
+                process_clause = socket_match.group('Process')
+                process_without_parens = paren_re.sub('', process_clause)
+                process_parts = process_without_parens.split(',')
 
-    return (listen_sockets, established_sockets)
+                if len(process_parts) % 3 != 0:
+                    raise ValueError('invalid process spec detected: "{}"',
+                                    process_clause)
+
+                for base in range(0, len(process_parts) // 3):
+                    individual_parts = process_parts[base*3:(base+1)*3]
+
+                    comm_match = comm_re.match(individual_parts[0])
+                    pid_match = pid_re.match(individual_parts[1])
+                    fd_match = fd_re.match(individual_parts[2])
+                    if (comm_match is None or
+                        pid_match is None or
+                        fd_match is None):
+                        raise ValueError('invalid process spec detected: "{}"',
+                                        process_clause)
+
+                    individual_socket.processes.append(SocketProcess(
+                        comm=comm_match.group(1),
+                        pid=int(pid_match.group(1)),
+                        fd=int(fd_match.group(1))
+                    ))
+
+                if socket_match.group('State') == 'LISTEN':
+                    self._listen_sockets.append(individual_socket)
+                elif socket_match.group('State') == 'ESTAB':
+                    self._established_sockets.append((
+                        individual_socket,
+                        ip_address(socket_match.group('PeerAddress')),
+                        int(socket_match.group('PeerPort'))
+                    ))
+
+    def _step_2_pair_loopback_peers(self):
+        """Identify established connection pairs to loopback addresses
+
+        This is the second phase of a multi-phase operation. At the end of
+        this phase, the _loopback_connections collection will have been
+        populated with instances that represent _established_sockets whose
+        peer addresses belong to the loopback interface.
+
+        Notably, the _loopback_connections may contain loopback connections
+        where the client/server directionality has been reversed (i.e., client
+        is shown as server, and server as client). This will be fixed in the
+        subsequent phase.
+        """
+
+        for (idx, socket_tuple) in enumerate(self._established_sockets):
+            (socket, peer_addr, peer_port) = socket_tuple
+
+            # Find the opposite side of the connection
+            range_start = idx + 1
+            range_end = len(self._established_sockets)
+            candidates = self._established_sockets[range_start:range_end]
+            for (candidate, candidate_addr, candidate_port) in candidates:
+                if (socket.addr == candidate_addr and
+                    socket.port == candidate_port and
+                    candidate.addr == peer_addr and
+                    candidate.port == peer_port):
+                    self._loopback_connections.append(LoopbackConnection(
+                        client=socket,
+                        server=candidate
+                    ))

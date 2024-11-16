1e41dfa31076a9679f03b82553f05298b56c4d03
Louis Wust <louis.w.wust@nasa.gov>
2024-11-16 11:48:33 -0800

    Fix some packaging descriptions and package names

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
 from setuptools import setup
 
+# https://packaging.python.org/en/latest/guides/making-a-pypi-friendly-readme/
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name="logind-idle-session-extras",
-    version="0.0.1",
-    description=("Refresh systemd-logind idle timeouts based on supplemental"
+    version="0.0.2",
+    description=("Refresh systemd-logind idle timeouts based on supplemental "
                  "user activity (e.g., VNC tunnel)"),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author="Louis Wust",
     author_email="louiswust@fastmail.fm",
     url="https://github.com/liverwust/logind-idle-session-extras",
+    packages=["logind_idle_session_extras"],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
@@ -16,5 +25,6 @@ setup(
         "Programming Language :: Python :: 3.6"
     ],
     license="MIT License",
+    package_dir={"": "src"},
     python_requires=">= 3.6"
 )

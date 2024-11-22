1450dfd9c6fae749ed60ee43cf9d991493faad2d
Louis Wust <louis.w.wust@nasa.gov>
2024-11-22 17:10:58 -0500

    Add new X11SessionProcesses "bag" object for DISPLAY

--- src/stop_idle_sessions/x11.py
+++ src/stop_idle_sessions/x11.py
@@ -1,57 +1,139 @@
 """X11 screen saver information to determine idle time"""
 
 
+from collections import defaultdict
+from datetime import timedelta
 import os
 import re
-from typing import Optional
+from typing import List, Mapping, Optional, Set, Tuple
 
 import Xlib.display
 import Xlib.error
 
 from .exception import SessionParseError
+from .ps import Process
 
 
-def parse_xauthority_cmdline(cmdline: str) -> Optional[str]:
-    """Attempt to identify an XAUTHORITY from the cmdline"""
+class X11SessionProcesses:
+    """Collect related Process objects and use them to determine X11 params
 
-    xvnc_auth_re = re.compile(r'^.*Xvnc.*-auth\s+(\S+).*$')
+    There are often many Process objects associated with a SystemD scope or
+    session. There may be one (or more!) instances of the DISPLAY or
+    XAUTHORITY variables among them. Some of their commandlines may even
+    provide clues as to these parameters.
 
-    xvnc_auth_match = xvnc_auth_re.match(cmdline)
-    if xvnc_auth_match is not None:
-        return xvnc_auth_match.group(1)
+    Once collected, these parameters can point to one or more DISPLAYs which
+    may provide an idle time (via the X11 Screen Saver extension).
+    """
 
-    return None
+    def __init__(self):
+        # Each "candidate tuple" associates a DISPLAY environment variable (or
+        # parsed command-line value) with an XAUTHORITY environment variable
+        # (or parsed command-line value). XAUTHORITY may be absent.
+        self._candidate_tuples: List[Tuple[str, Optional[str]]] = []
 
-def retrieve_idle_time_ms(display: str,
-                          xauthority: Optional[str] = None) -> int:
-    """Retrieve the idle time (in milliseconds) for the given X11 DISPLAY"""
+    def add(self, process: Process):
+        """Add information from a Process to the internal tracking list
 
-    # Crazy hack to try and work around this issue, reported by a _different
-    # project_ (which has never made it into the python-xlib upstream):
-    # https://github.com/asweigart/pyautogui/issues/202
-    extensions = getattr(Xlib.display, 'ext').__extensions__
-    if ('RANDR', 'randr') in extensions:
-        extensions.remove(('RANDR', 'randr'))
-    if ('XFIXES', 'xfixes') in extensions:
-        extensions.remove(('XFIXES', 'xfixes'))
+        This will extract information from the given Process which will allow
+        the "candidate tuples" list to expand and incorporate the new info.
+        The Processes are not actually collected internally per se -- just
+        relevant information.
+        """
 
-    try:
-        if xauthority is not None:
-            os.environ['XAUTHORITY'] = xauthority
+        # Try some specific command lines
+        xvnc_match = X11SessionProcesses.parse_xvnc_cmdline(process.cmdline)
 
-        d = Xlib.display.Display(display)
-        return d.screen().root.screensaver_query_info().idle
+        display: Optional[str] = None
+        xauthority: Optional[str] = None
 
-    except Xlib.error.DisplayConnectionError as err:
-        raise SessionParseError(f'Could not connect to X11 display identified '
-                                f'by "{display}"') from err
+        if xvnc_match[0] is not None:
+            display = xvnc_match[0]
+        elif 'DISPLAY' in process.environ:
+            display = process.environ['DISPLAY']
 
-    except Xlib.error.ConnectionClosedError as err:
-        raise SessionParseError(f'Could not maintain a connection to the X11 '
-                                f'display identified by "{display}"') from err
+        if xvnc_match[1] is not None:
+            xauthority = xvnc_match[1]
+        elif 'XAUTHORITY' in process.environ:
+            xauthority = process.environ['XAUTHORITY']
 
-    except AttributeError as err:
-        raise SessionParseError(f'Cannot access attributes from X11 server '
-                                f'responses associated with display '
-                                f'"{display}", probably due to a broken or '
-                                f'erroneous connection') from err
+        if display is not None:
+            self._candidate_tuples.append((display, xauthority))
+
+    def get_all_candidates(self) -> List[Tuple[str, Optional[str]]]:
+        """Review each of the candidates tuples for DISPLAY/XAUTHORITY pairs
+
+        Every DISPLAY can be tried without any XAUTHORITY. If a given
+        XAUTHORITY shows up for a DISPLAY, then return it as a candidate.
+        """
+
+        display_xauthorities: Mapping[str,
+                                      Set[Optional[str]]] = defaultdict(set)
+
+        for display, xauthority in self._candidate_tuples:
+            display_xauthorities[display].add(xauthority)
+
+        # Make sure that we try XAUTHORITY = None for each of these
+        for xauthority_set in display_xauthorities.values():
+            if None not in xauthority_set:
+                xauthority_set.add(None)
+
+        resulting_list: List[Tuple[str, Optional[str]]] = []
+        for display, xauthority_set in display_xauthorities.items():
+            for xauthority in xauthority_set:
+                resulting_list.append((display, xauthority))
+
+        return resulting_list
+
+    @staticmethod
+    def parse_xvnc_cmdline(cmdline: str) -> Tuple[Optional[str],
+                                                  Optional[str]]:
+        """Attempt to identify information from an Xvnc command line
+
+        The first element of the returned tuple is a candidate DISPLAY, if one
+        is found. The second is a candidate XAUTHORITY, if one is found.
+        """
+
+        xvnc_re = re.compile(r'^.*Xvnc\s+(:[0-9]+).*-auth\s+(\S+).*$')
+
+        xvnc_match = xvnc_re.match(cmdline)
+        if xvnc_match is not None:
+            return (xvnc_match.group(1), xvnc_match.group(2))
+
+        return (None, None)
+
+    @staticmethod
+    def retrieve_idle_time_ms(display: str,
+                              xauthority: Optional[str] = None) -> timedelta:
+        """Retrieve the idle time (in milliseconds) for the given X11 DISPLAY"""
+
+        # Crazy hack to try and work around this issue, reported by a _different
+        # project_ (which has never made it into the python-xlib upstream):
+        # https://github.com/asweigart/pyautogui/issues/202
+        extensions = getattr(Xlib.display, 'ext').__extensions__
+        if ('RANDR', 'randr') in extensions:
+            extensions.remove(('RANDR', 'randr'))
+        if ('XFIXES', 'xfixes') in extensions:
+            extensions.remove(('XFIXES', 'xfixes'))
+
+        try:
+            if xauthority is not None:
+                os.environ['XAUTHORITY'] = xauthority
+
+            d = Xlib.display.Display(display)
+            idle_time_ms = d.screen().root.screensaver_query_info().idle
+            return timedelta(milliseconds=idle_time_ms)
+
+        except Xlib.error.DisplayConnectionError as err:
+            raise SessionParseError(f'Could not connect to X11 display identified '
+                                    f'by "{display}"') from err
+
+        except Xlib.error.ConnectionClosedError as err:
+            raise SessionParseError(f'Could not maintain a connection to the X11 '
+                                    f'display identified by "{display}"') from err
+
+        except AttributeError as err:
+            raise SessionParseError(f'Cannot access attributes from X11 server '
+                                    f'responses associated with display '
+                                    f'"{display}", probably due to a broken or '
+                                    f'erroneous connection') from err
--- tests/test_x11.py
+++ tests/test_x11.py
@@ -3,20 +3,74 @@
 
 from unittest import TestCase
 
-import stop_idle_sessions.x11
+from stop_idle_sessions.list_set import compare_list_sets
+from stop_idle_sessions.ps import Process
+from stop_idle_sessions.x11 import X11SessionProcesses
 
 
-class ParseXauthorityTestCase(TestCase):
-    """Verify the behavior of the xauth command line parser"""
+class ParseCommandLineTestCase(TestCase):
+    """Verify the behavior of the DISPLAY/XAUTHORITY command line parser"""
 
-    def test_parse_xauthority_cmdline(self):
-        """Verify the behavior of the xauth command line parser"""
+    def test_parse_xvnc_cmdline(self):
+        """Verify the behavior of the Xvnc command line parser"""
 
         sample_cmdline = "/usr/bin/Xvnc :1 -auth /home/louis/.Xauthority -desktop samplehost.sampledomain:1 (auser) -fp catalogue:/etc/X11/fontpath.d -geometry 1024x768 -pn -rfbauth /home/auser/.vnc/passwd -rfbport 5901 -localhost"
-        expected_xauthority = '/home/louis/.Xauthority'
+        expected_results = (':1', '/home/louis/.Xauthority')
 
-        actual_xauthority = stop_idle_sessions.x11.parse_xauthority_cmdline(
+        actual_results = X11SessionProcesses.parse_xvnc_cmdline(
                 sample_cmdline
         )
 
-        self.assertEqual(expected_xauthority, actual_xauthority)
+        self.assertEqual(expected_results, actual_results)
+
+
+class X11SessionProcessesTestCase(TestCase):
+    """Verify correct accumulation and handling of process information"""
+
+    def test_normal_collection_of_vnc_processes(self):
+        """This is a common case where a few VNC processes share a DISPLAY"""
+
+        bag = X11SessionProcesses()
+        processes = [
+            Process(
+                    pid=20272,
+                    cmdline=('/usr/bin/Xvnc :1 -auth /home/auser/.Xauthority '
+                             '-desktop remotehost.remotedomain (auser) -fp '
+                             'catalogue:/etc/X11/fontpath.d -geometry 1024x768 '
+                             '-pn -rfbauth /home/auser/.vnc/passwd -rfbport '
+                             '5901 -localhost'),
+                    environ={}
+            ),
+            Process(
+                    pid=20277,
+                    cmdline='/bin/sh /home/louis/.vnc/xstartup',
+                    environ={
+                        'DISPLAY': ':1'
+                    }
+            ),
+            Process(
+                    pid=20278,
+                    cmdline='/usr/libexec/gnome-session-binary',
+                    environ={
+                        'DISPLAY': ':1'
+                    }
+            ),
+            Process(
+                    pid=20373,
+                    cmdline='/usr/bin/gnome-shell',
+                    environ={
+                        'DISPLAY': ':1'
+                    }
+            )
+        ]
+
+        for process in processes:
+            bag.add(process)
+
+        expected_candidates = [
+                (':1', None),
+                (':1', '/home/auser/.Xauthority')
+        ]
+        actual_candidates = bag.get_all_candidates()
+        self.assertTrue(compare_list_sets(expected_candidates,
+                                          actual_candidates))
